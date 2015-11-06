53f870d93d509406cfb91d2f1d7f52801408f5d9
Mark Stier <stier020@Marks-MacBook-Pro.local>
2015-11-05 19:01:44 -0600

    init

---
+++ index.js
@@ -0,0 +1,29 @@
+"use strict"
+
+var Worker = require("basic-distributed-computation").Worker;
+
+class Copy extends Worker {
+  constructor(parent){
+    super("mv", parent);
+  }
+
+  work(req, inputKey, outputKey){
+    var inputVal;
+    if(inputKey){
+      inputVal = req.body[inputKey];
+    } else {
+      inputVal = req.body;
+    }
+    if(outputKey && inputKey){
+      req.body[outputKey] = inputVal;
+    } else if(outputKey){
+      req.body = {};
+      req.body[outputKey] = inputVal;
+    } else {
+      req.body = inputVal;
+    }
+    req.next();
+  }
+}
+
+module.exports = Copy;
